460b7b4ae1dba550f9d8f353939f78a797e7ac88
Moi-meme <maximouth@maximouth.home>
2016-12-04 14:32:41 +0100

    debut de ce qu'on va devoir faire + makefile

--- serveur.c
+++ serveur.c
@@ -15,53 +15,68 @@ void *fn_thread_http(void *requete) {
 }
 
 int main (int argc, char ** argv) {
-  int num_port = 0;
+  /* arguments passé en ligen de commande  */
+  int num_port  = 0;
   int nb_client = 0;
-  int num_cpt 0;
+  int num_cpt   = 0;
+
+  /* descripteur de la socket */
   int sockd;
-  int cpt = 0;
+
+  /* structure qui contient les informations de la socket */
   struct sockaddr_in serv;
   
-  
+  /* nombre de client en simultané */
+  int cpt = 0;
+
+  /* verification si l'appel au programme est bon  */
   if (argc != 4) {
-    perror ("mauvaise utilisation\n");
+    perror ("mauvaise utilisation\n [port] [nbclientmax] [q5]");
     exit (1);
   }
-  
+
+  /* recuperation des variables  */
   num_port   = atoi (argv[1]);
-  nb_client = atoi (argv[2]);
+  nb_client  = atoi (argv[2]);
   num_cpt    = atoi (argv[3]);
 
-  /* Creation de la socket */
-  /* TODO chose between AF_UNIX and AF_INET 
-     unlink and/or closesocket() */
-  if((sockd = socket(AF_UNIX, SOCK_DGRAM, 0)) == INVALID_SCOKET) {
-      perror("Socket\n");
-      exit(1);
-  }
-      
-   /* remplir le nom   */
-  memset ((char*) &serv, 0, sizeof(serv));
-  serv.sin_addr.s_addr = htonl (INADDR_ANY);
-  serv.sin_port = htons (num_port);
-  serv.sin_family = AF_UNIX;
 
-  unlink (
+  /*   variable partagées dans les threads
+       -> mettre un mutex pour la gestion du compteur de clients
+
+       ->creer la socket en UDP
+       ->remplir les informations
+       ->faire un bind pour les lier (detruire la connextion existance 
+       si il en existe une
+       
+       lire en continue de flux dentrée
+       une fois un message bien formé arrive :
+           -> incrementer cpt, test et tout
+	   -> creer un thread avec le message en argument
+       sinon rien 
+
+
+       dans le thread :
+       -> decoder le message
+       -> chercher le fichier
+          -> si il existe et accesible: 
+	     repondre : "HTTP/1.1 200 OK" 
+          -> si il existe et pas les droit: 
+	     repondre : "HTTP/1.1 403 Forbidden" 
+          -> si il n'existe pas: 
+	     repondre : "HTTP/1.1 404 not found" 
+       
+       -> chercher dans le fichier mime.types le typer de fichier
+          repondre : Content-Type: (le type trouvé)
+
+       -> envoyer la reponse au client (ip dans la requette)
+
+       -> decrementer cpt (mutex et tout)
+
+       -> finir le thread
+
+    */
   
-  
-  /* nomage */
-  if (bind (sockd, (struct sockaddr *) &serv, sizeof(serv)) < 0) {
-    perror ("bind");
-    exit (1);
-  }
-
-
-  
-  while (1) {
-
-
-  }
-
   
   return 0;
 }
